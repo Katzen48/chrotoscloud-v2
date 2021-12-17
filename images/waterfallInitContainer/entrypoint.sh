a997eb4d387ef6e0dfd8f09109e58add92e7a46d
Katzen48 | Tobi <Katzen48@freenet.de>
2021-12-17 17:15:21 +0100

    added waterfallInitContainer

---
+++ images/waterfallInitContainer/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+node .
+mkdir /workdir/plugins/
+cp bungeecord-all.jar /workdir/plugins/bungeecord-all.jar
---
+++ images/waterfallInitContainer/main.js
@@ -0,0 +1,29 @@
+// Fetch newest paper version
+
+const BASE_URL = 'https://papermc.io/api/v2/projects/waterfall/versions';
+const path = '/workdir'
+const fetch = require('cross-fetch')
+const fs = require('fs');
+const https = require('https');
+
+(async () => {
+    let version = process.env.PAPER_VERSION || '1.18';
+
+    let versionResponse = await fetch(`${BASE_URL}/${version}`)
+    let json = await versionResponse.json();
+    let newestBuild = json.builds[json.builds.length - 1];
+
+    await new Promise((resolve, reject) => {
+        https.get(`${BASE_URL}/${version}/builds/${newestBuild}/downloads/waterfall-${version}-${newestBuild}.jar`, (res) => {
+            const stream = fs.createWriteStream(path + '/server.jar');
+            res.pipe(stream);
+
+            stream.on('finish', () => {
+                stream.close();
+                resolve();
+            });
+        })
+    });
+    
+    fs.writeFileSync(path + '/eula.txt', 'eula=true');
+})();
