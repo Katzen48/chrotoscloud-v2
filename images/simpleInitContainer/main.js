5bd531d8c283bec604663cbdcc0f1d021b719791
Katzen48 | Tobi <Katzen48@freenet.de>
2021-12-19 22:19:49 +0100

    updated kubernetes definitions, optimized images

--- images/simpleInitContainer/main.js
+++ images/simpleInitContainer/main.js
@@ -1,20 +1,21 @@
 // Fetch newest paper version
 
-const BASE_URL = 'https://papermc.io/api/v2/projects/velocity/versions';
+const BASE_URL = 'https://papermc.io/api/v2/projects/';
 const path = '/workdir'
 const fetch = require('cross-fetch')
 const fs = require('fs');
 const https = require('https');
 
 (async () => {
-    let version = process.env.PAPER_VERSION || '3.1.0';
+    const VERSIONS_URL = BASE_URL + process.env.SERVER_SOFTWARE + '/versions';
+    const VERSION = process.env.SERVER_SOFTWARE_VERSION;
 
-    let versionResponse = await fetch(`${BASE_URL}/${version}`)
+    let versionResponse = await fetch(`${VERSIONS_URL}/${VERSION}`)
     let json = await versionResponse.json();
     let newestBuild = json.builds[json.builds.length - 1];
 
-    await new Promise((resolve, reject) => {
-        https.get(`${BASE_URL}/${version}/builds/${newestBuild}/downloads/velocity-${version}-${newestBuild}.jar`, (res) => {
+    await new Promise((resolve) => {
+        https.get(`${VERSIONS_URL}/${VERSION}/builds/${newestBuild}/downloads/paper-${VERSION}-${newestBuild}.jar`, (res) => {
             const stream = fs.createWriteStream(path + '/server.jar');
             res.pipe(stream);
 
