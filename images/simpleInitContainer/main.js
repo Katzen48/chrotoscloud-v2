5c350ef077730d6a83fbff956e7542e2169a5788
Katzen48 | Tobi <Katzen48@freenet.de>
2021-12-19 22:33:35 +0100

    added logging

--- images/simpleInitContainer/main.js
+++ images/simpleInitContainer/main.js
@@ -10,9 +10,13 @@ const https = require('https');
     const VERSIONS_URL = BASE_URL + process.env.SERVER_SOFTWARE + '/versions';
     const VERSION = process.env.SERVER_SOFTWARE_VERSION;
 
+    console.log('Fetching newest Build of', process.env.SERVER_SOFTWARE, 'of version', VERSION);
+
     let versionResponse = await fetch(`${VERSIONS_URL}/${VERSION}`)
     let json = await versionResponse.json();
     let newestBuild = json.builds[json.builds.length - 1];
+
+    console.log('Starting download of build', newestBuild);
 
     await new Promise((resolve) => {
         https.get(`${VERSIONS_URL}/${VERSION}/builds/${newestBuild}/downloads/paper-${VERSION}-${newestBuild}.jar`, (res) => {
