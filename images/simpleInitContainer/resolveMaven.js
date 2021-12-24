cd18a75b9d4e378f43bc778467863e9a3d6bc73f
Katzen48 | Tobi <Katzen48@freenet.de>
2021-12-24 02:01:10 +0100

    replaced maven parser

--- images/simpleInitContainer/resolveMaven.js
+++ images/simpleInitContainer/resolveMaven.js
@@ -1,12 +1,13 @@
 const fetch = require('cross-fetch')
-const parser = require('pom-parser');
+const { XMLParser } = require("fast-xml-parser");
+const parser = new XMLParser({ignoreAttributes: true, preserveOrder: true});
 
 module.exports = async function(repository, groupId, artifactId, version, user, password) {
     await new Promise((resolve, reject) => {
         let artifactUrl = repository + '/' + groupId.replaceAll('.', '/') + '/' + artifactId + '/' + version;
-        console.log('Requesting maven-metadata from\'', artifactUrl, '\'');
 
         let metadataUrl = artifactUrl + '/maven-metadata.xml';
+        console.log('Requesting maven-metadata from\'' + metadataUrl + '\'');
         let promise;
         if (user && password) {
             let headers = new fetch.Headers();
@@ -20,28 +21,21 @@ module.exports = async function(repository, groupId, artifactId, version, user, 
 
         promise.then(xmlContent => {
             console.log('Parsing metadata')
-            parser.parse({
-                xmlContent
-            }, function (err, pomResponse) {
-                if (err) {
-                    console.log("ERROR: " + err);
-                    reject(err);
-                }
+            let pomResponse = parser.parse(xmlContent);
 
-                let versionString = 'core-';
-                if (version.endsWith('-SNAPSHOT')) {
-                    versionString += version.substr(0, version.lastIndexOf('-SNAPSHOT') + 2);
-                    versionString += pomResponse.pomObject.versioning.snapshot.timestamp + '-' +
-                        pomResponse.pomObject.versioning.snapshot.buildNumber;
-                } else {
-                    versionString += pomResponse.pomObject.release;
-                }
+            let versionString = 'core-';
+            if (version.endsWith('-SNAPSHOT')) {
+                versionString += version.substr(0, version.lastIndexOf('-SNAPSHOT') + 2);
+                versionString += pomResponse.metadata.versioning.snapshot.timestamp + '-' +
+                    pomResponse.metadata.versioning.snapshot.buildNumber;
+            } else {
+                versionString += pomResponse.metadata.release;
+            }
 
-                versionString += '-all.jar';
+            versionString += '-all.jar';
 
-                console.log('Version String:', versionString);
-                resolve(artifactUrl + '/' + versionString);
-            })
+            console.log('Version String:', versionString);
+            resolve(artifactUrl + '/' + versionString);
         }).catch(error => reject(error));
     });
 }
