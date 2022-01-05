6af0992c84ab964ab254efd81072fa30652e5ff5
Katzen48 | Tobi <Katzen48@freenet.de>
2022-01-06 00:07:34 +0100

    unregistered reload command, as it is no compatible with the chrotoscloud plugin

--- plugin/plugin/paper/src/main/java/net/chrotos/chrotoscloud/paper/CloudPlugin.java
+++ plugin/plugin/paper/src/main/java/net/chrotos/chrotoscloud/paper/CloudPlugin.java
@@ -1,11 +1,15 @@
 package net.chrotos.chrotoscloud.paper;
 
 import net.chrotos.chrotoscloud.Cloud;
+import org.bukkit.command.Command;
 import org.bukkit.plugin.PluginLoadOrder;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.java.annotation.plugin.LoadOrder;
 import org.bukkit.plugin.java.annotation.plugin.Plugin;
 import org.bukkit.plugin.java.annotation.plugin.author.Author;
+
+import java.lang.reflect.Field;
+import java.util.Map;
 
 @Plugin(name = "ChrotosCloud", version = "3.0-SNAPSHOT")
 @Author("Katzen48")
@@ -27,5 +31,22 @@ public class CloudPlugin extends JavaPlugin {
     public void onEnable() {
         cloud.initialize();
         getServer().getPluginManager().registerEvents(new PaperEventHandler(cloud), this);
+
+        tryUnregisterReloadCommands();
+    }
+
+    private void tryUnregisterReloadCommands() {
+        try {
+            Field knownCommandsField = org.bukkit.command.SimpleCommandMap.class.getDeclaredField("knownCommands");
+            knownCommandsField.setAccessible(true);
+
+            Map<String, Command> knownCommands = (Map<String, Command>) knownCommandsField.get(getServer().getCommandMap());
+            knownCommands.remove("reload");
+            knownCommands.remove("bukkit:reload");
+            knownCommands.remove("spigot:reload");
+            knownCommands.remove("paper:paper");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
