0a246b9cc2d17ec55fe5f689ba9cfe0bf38ceef1
Katzen48 | Tobi <Katzen48@freenet.de>
2022-02-10 21:40:41 +0100

    fixed config env substitution

--- images/velocityInitContainer/entrypoint.sh
+++ images/velocityInitContainer/entrypoint.sh
@@ -3,4 +3,4 @@ node .
 mkdir -p /workdir/plugins/
 cp velocity-all.jar /workdir/plugins/velocity-all.jar
 
-envsubst "/etc/config/velocity.toml" > "/workdir/velocity.toml"
+envsubst < /etc/config/velocity.toml > /workdir/velocity.toml
