df64b5b1b808e6bfbc341fe55aad73deeec0664d
Katzen48 | Tobi <Katzen48@freenet.de>
2022-02-12 00:42:59 +0100

    added player count synchronization

--- plugin/api/src/main/java/net/chrotos/chrotoscloud/messaging/pubsub/Registration.java
+++ plugin/api/src/main/java/net/chrotos/chrotoscloud/messaging/pubsub/Registration.java
@@ -1,6 +1,6 @@
 package net.chrotos.chrotoscloud.messaging.pubsub;
 
-public interface Registration {
+public interface Registration extends AutoCloseable {
     void subscribe(String... channels);
     void unsubscribe(String... channels);
     boolean isSubscribed();
