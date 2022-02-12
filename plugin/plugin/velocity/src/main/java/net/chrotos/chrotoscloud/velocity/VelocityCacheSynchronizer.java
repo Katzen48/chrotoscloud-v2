1255db1d81f172cbe4a50e697f05b719955f6907
Katzen48 | Tobi <Katzen48@freenet.de>
2022-02-12 12:20:26 +0100

    velocity: added player count caching

--- plugin/plugin/velocity/src/main/java/net/chrotos/chrotoscloud/velocity/VelocityCacheSynchronizer.java
+++ plugin/plugin/velocity/src/main/java/net/chrotos/chrotoscloud/velocity/VelocityCacheSynchronizer.java
@@ -8,40 +8,33 @@ import com.velocitypowered.api.event.connection.DisconnectEvent;
 import com.velocitypowered.api.event.connection.LoginEvent;
 import com.velocitypowered.api.event.connection.PostLoginEvent;
 import com.velocitypowered.api.event.proxy.ProxyPingEvent;
-import com.velocitypowered.api.proxy.server.ServerPing;
 import com.velocitypowered.api.scheduler.ScheduledTask;
 import com.velocitypowered.api.scheduler.TaskStatus;
 import lombok.RequiredArgsConstructor;
-import net.chrotos.chrotoscloud.messaging.pubsub.Registration;
 import net.kyori.adventure.text.Component;
 
 import java.time.Duration;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class VelocityCacheSynchronizer {
     private final Set<String> proxies = Sets.newConcurrentHashSet();
     private final CloudPlugin plugin;
-    private Registration pubSub;
     private ScheduledTask checkTask;
     private String proxyKey;
+    private int playerCount;
 
     public void initialize() {
         proxyKey = "proxy:" + System.getenv("HOSTNAME") + ":players";
 
-        pubSub = plugin.cloud.getPubSub().register(this::onPubSub, "proxy:register");
         checkTask = plugin.proxyServer.getScheduler()
                                         .buildTask(plugin, this::checkProxies)
                                         .repeat(5L, TimeUnit.SECONDS).schedule();
-        plugin.cloud.getPubSub().publish("proxy:register", System.getenv("HOSTNAME"));
     }
 
     public void destruct() {
-        pubSub.unsubscribe("proxy:register");
-
         if (checkTask.status() == TaskStatus.SCHEDULED) {
             checkTask.cancel();
         }
@@ -94,7 +87,7 @@ public class VelocityCacheSynchronizer {
     }
 
     public int getPlayerCount() {
-        return (int) proxies.stream().mapToLong((proxy) -> plugin.cloud.getCache().setSize("proxy:" + proxy + ":players")).sum();
+        return playerCount;
     }
 
     public Set<String> getPlayers() {
@@ -107,14 +100,6 @@ public class VelocityCacheSynchronizer {
         return players;
     }
 
-    private void onPubSub(String channel, String message) {
-        if (channel.equalsIgnoreCase("proxy:register")) {
-            if (!message.equals(System.getenv("HOSTNAME"))) {
-                proxies.add(message);
-            }
-        }
-    }
-
     private void checkProxies() {
         Set<String> keys = plugin.cloud.getCache().keys("proxy:*:players");
 
@@ -122,5 +107,8 @@ public class VelocityCacheSynchronizer {
         keys.forEach((key) -> proxies.add(key.split(":")[1]));
 
         plugin.cloud.getCache().expire(proxyKey, Duration.ofSeconds(10L));
+
+        playerCount = (int) proxies.stream().mapToLong(
+                (proxy) -> plugin.cloud.getCache().setSize("proxy:" + proxy + ":players")).sum();
     }
 }
