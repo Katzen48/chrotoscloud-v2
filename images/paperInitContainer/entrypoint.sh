3aea637579b54ea991b1f0a6a69438811e3e9e4f
Katzen48 | Tobi <Katzen48@freenet.de>
2022-03-23 19:05:37 +0100

    fixed unzip of plugin configuration

--- images/paperInitContainer/entrypoint.sh
+++ images/paperInitContainer/entrypoint.sh
@@ -3,6 +3,7 @@ node main.js
 cd /workdir/worlds
 find . -name "*.zip" -exec unar -q {} \;
 
-find plugins -name "*.zip" -exec unar -q {} \;
+cd ../plugins
+find . -name "*.zip" -exec unar -q {} \;
 
 envsubst < /etc/config/paper.yml > /workdir/paper.yml
