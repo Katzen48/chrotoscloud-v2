3d8511cd2b2962f3ff10a21b0d375406d42b4f2d
Katzen48 | Tobi <Katzen48@freenet.de>
2022-03-25 15:53:00 +0100

    fixed pod lookup, fixed game mode lookup

--- plugin/plugin/velocity/src/main/java/net/chrotos/chrotoscloud/velocity/games/VelocityGameManager.java
+++ plugin/plugin/velocity/src/main/java/net/chrotos/chrotoscloud/velocity/games/VelocityGameManager.java
@@ -2,6 +2,13 @@ package net.chrotos.chrotoscloud.velocity.games;
 
 import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.server.RegisteredServer;
+import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.ApiException;
+import io.kubernetes.client.openapi.Configuration;
+import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.openapi.models.V1Pod;
+import io.kubernetes.client.openapi.models.V1PodList;
+import io.kubernetes.client.util.Config;
 import lombok.NonNull;
 import net.chrotos.chrotoscloud.games.*;
 import net.chrotos.chrotoscloud.games.events.*;
@@ -14,21 +21,36 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class VelocityGameManager implements GameManager, AutoCloseable {
     private final VelocityCloud cloud;
+    private final CoreV1Api coreV1Api;
     private Registration<GameServerLookupRequest, Void> lookup;
     private Registration<GameServerPingRequest, Void> ping;
     private Registration<PlayerTeleportToServerRequest, Void> teleport;
 
     public VelocityGameManager(VelocityCloud cloud) {
         this.cloud = cloud;
+
+        try {
+            ApiClient k8sClient = Config.defaultClient();
+            Configuration.setDefaultApiClient(k8sClient);
+            this.coreV1Api = new CoreV1Api();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public CompletableFuture<GameServer> getGameServer(@NonNull String name) {
+        return getGameServer(name, null);
+    }
+
+    private CompletableFuture<GameServer> getGameServer(@NonNull String name, String gameMode) {
         RegisteredServer server = cloud.getProxyServer().getServer(name).orElse(null);
 
         CompletableFuture<GameServer> future = new CompletableFuture<>();
@@ -40,14 +62,18 @@ public class VelocityGameManager implements GameManager, AutoCloseable {
             return server.ping().thenApply(ping -> {
                 int maxPlayers = 0;
                 int playerCount = 0;
-                String gameMode = name.substring(0, name.lastIndexOf("-"));
 
                 if (ping.getPlayers().isPresent()) {
                     maxPlayers = ping.getPlayers().get().getMax();
                     playerCount = ping.getPlayers().get().getOnline();
                 }
 
-                return new CloudGameServer(name, maxPlayers, playerCount, gameMode);
+                String podGameMode = gameMode;
+                if (podGameMode == null) {
+                    podGameMode = getPodGameMode(name);
+                }
+
+                return new CloudGameServer(name, maxPlayers, playerCount, podGameMode);
             });
         }
     }
@@ -64,17 +90,21 @@ public class VelocityGameManager implements GameManager, AutoCloseable {
 
     public CompletableFuture<List<GameServer>> getGameServersByGameMode(String gameMode) {
         return CompletableFuture.supplyAsync(() -> {
-            Collection<RegisteredServer> servers = cloud.getProxyServer().getAllServers();
+                Collection<RegisteredServer> servers;
+            if (gameMode == null) {
+                servers = cloud.getProxyServer().getAllServers();
+            } else {
+                List<String> serverNames = getPodsByGameMode(gameMode);
+                servers = serverNames.stream().map(serverName -> cloud.getProxyServer().getServer(serverName).orElse(null))
+                                    .filter(Objects::nonNull).collect(Collectors.toList());
+            }
+
             CompletableFuture[] gameServers = new CompletableFuture[servers.size()];
 
             AtomicInteger i = new AtomicInteger();
             servers.forEach(server -> {
                 String name = server.getServerInfo().getName();
-                if (gameMode != null && !name.startsWith(gameMode + "-")) {
-                    return;
-                }
-
-                gameServers[i.getAndIncrement()] = getGameServer(name);
+                gameServers[i.getAndIncrement()] = gameMode != null ? getGameServer(name, gameMode) : getGameServer(name);
             });
 
             ArrayList<GameServer> list = new ArrayList<>();
@@ -92,7 +122,7 @@ public class VelocityGameManager implements GameManager, AutoCloseable {
 
     @Override
     public CompletableFuture<GameServer> getRandom(@NonNull String gameMode) {
-        return getGameServers().thenApply(gameServers -> {
+        return getGameServers(gameMode).thenApply(gameServers -> {
             if (gameServers.size() > 0) {
                 return gameServers.get((int) (Math.random() * gameServers.size()));
             }
@@ -110,6 +140,39 @@ public class VelocityGameManager implements GameManager, AutoCloseable {
         lookup = cloud.getQueue().register(onLookup(), "games.server.lookup");
         ping = cloud.getQueue().register(onPing(), "games.server.ping");
         teleport = cloud.getQueue().register(onTeleport(), "player.teleport.server");
+    }
+
+    protected List<String> getPodsByGameMode(@NonNull String gameMode) {
+        try {
+            ArrayList<String> pods = new ArrayList<>();
+            V1PodList list = coreV1Api.listNamespacedPod("servers", null, false, null,
+                    null, "net.chrotos.chrotoscloud.gameserver/gamemode=" + gameMode, null, null,
+                    null, null);
+
+            for (V1Pod pod : list.getItems()) {
+                pods.add(pod.getMetadata().getName());
+            }
+
+            return pods;
+        } catch (ApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected String getPodGameMode(@NonNull String podName) {
+        try {
+            V1PodList list = coreV1Api.listNamespacedPod("servers", null, false, null,
+                    "metadata.name=" + podName, null, null, null,
+                    null, null);
+
+            if (list.getItems().isEmpty()) {
+                return null;
+            }
+
+            return list.getItems().get(0).getMetadata().getLabels().get("net.chrotos.chrotoscloud.gameserver/gamemode");
+        } catch (ApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private Listener<GameServerLookupRequest, Void> onLookup() {
