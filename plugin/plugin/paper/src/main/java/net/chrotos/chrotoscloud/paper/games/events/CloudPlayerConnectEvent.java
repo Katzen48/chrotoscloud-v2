f0e1dbd7d459ef86447468da10cb671a4e8dcf5f
Katzen48 | Tobi <Katzen48@freenet.de>
2022-04-17 18:30:59 +0200

    fixed CloudPlayerConnectEvent

--- plugin/plugin/paper/src/main/java/net/chrotos/chrotoscloud/paper/games/events/CloudPlayerConnectEvent.java
+++ plugin/plugin/paper/src/main/java/net/chrotos/chrotoscloud/paper/games/events/CloudPlayerConnectEvent.java
@@ -27,4 +27,8 @@ public class CloudPlayerConnectEvent extends Event {
     public @NotNull HandlerList getHandlers() {
         return HANDLERS;
     }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS;
+    }
 }
