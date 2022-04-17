b569a6f70b6a7cb1b61716b748efeeb0a531532b
Katzen48 | Tobi <Katzen48@freenet.de>
2022-04-17 20:55:21 +0200

    fixed CloudPlayerConnectEvent

--- plugin/plugin/paper/src/main/java/net/chrotos/chrotoscloud/paper/games/events/CloudPlayerConnectEvent.java
+++ plugin/plugin/paper/src/main/java/net/chrotos/chrotoscloud/paper/games/events/CloudPlayerConnectEvent.java
@@ -10,17 +10,12 @@ import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 
 @Getter
+@RequiredArgsConstructor
 public class CloudPlayerConnectEvent extends Event {
     private static final HandlerList HANDLERS = new HandlerList();
     private final GameServer from;
     @NonNull
     private final Player player;
-
-    public CloudPlayerConnectEvent(GameServer from, @NonNull Player player) {
-        super(true);
-        this.from = from;
-        this.player = player;
-    }
 
     public boolean isNewConnection() {
         return from == null;
