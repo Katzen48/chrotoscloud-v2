baf6cfa0193608e10096e1861999630c01f18e29
Katzen48 | Tobi <Katzen48@freenet.de>
2022-10-14 17:20:04 +0200

    fixed comparator

--- plugin/plugin/paper/src/main/java/net/chrotos/chrotoscloud/paper/games/queue/PaperLeastPlayersQueueManager.java
+++ plugin/plugin/paper/src/main/java/net/chrotos/chrotoscloud/paper/games/queue/PaperLeastPlayersQueueManager.java
@@ -9,6 +9,7 @@ import net.chrotos.chrotoscloud.games.QueueManager;
 import net.chrotos.chrotoscloud.games.QueueMode;
 import net.chrotos.chrotoscloud.player.Player;
 
+import java.util.Comparator;
 import java.util.concurrent.CompletableFuture;
 
 @RequiredArgsConstructor
@@ -29,6 +30,6 @@ public class PaperLeastPlayersQueueManager implements QueueManager {
     public CompletableFuture<? extends GameServer> getServer(@NonNull Player player) {
         return gameManager.getGameServers(getGameMode()).thenApply(gameServers ->
                 gameServers.stream().filter(server -> server.getMaxPlayers() == 0 || server.getPlayerCount() < server.getMaxPlayers() - 2)
-                        .min((s1, s2) -> Integer.compare(s2.getPlayerCount(), s1.getPlayerCount())).orElse(null));
+                        .min(Comparator.comparing(GameServer::getPlayerCount)).orElse(null));
     }
 }
